0f319eca5155e6b2185a572639d9ca0cd1c726c8
yoga muhamad <yogaandhika14@gmail.com>
2020-12-03 17:27:50 +0700

    logout

--- src/pages/UserProfile/index.js
+++ src/pages/UserProfile/index.js
@@ -1,23 +1,38 @@
-import React, { useState,useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { StyleSheet, View } from 'react-native'
 import { ILNullPhoto } from '../../assets';
 import { Gap, Header, List, Profile } from '../../components';
-import {getData} from '../../utils';
+import { colors, getData } from '../../utils';
+import { Fire } from '../../config';
+import { showMessage } from 'react-native-flash-message';
 const index = ({ navigation }) => {
     const [profile, setProfile] = useState({
         fullName: '',
         profesional: '',
-        photo:ILNullPhoto,
+        photo: ILNullPhoto,
     })
     useEffect(() => {
         getData('user')
             .then(res => {
                 const data = res;
-                data.photo ={uri: res.photo}
+                data.photo = { uri: res.photo }
                 setProfile(data);
             });
     }, [])
-
+    //funsi logout
+    const singOut = () => {
+        Fire.auth().signOut().then(() => {
+            console.log*'succes SignOut'
+            navigation.replace('GetStarted')
+        }).catch(err => {
+            showMessage({
+                message:err.message,
+                type:'default',
+                backgroundColor:colors.error,
+                color: 'white'
+            })
+        })
+    }
 
     //jika list sudah terpanggil dan tidak nampak cek di componen molecus apa saja atribut props yang harus dimiliki
     return (
@@ -26,16 +41,16 @@ const index = ({ navigation }) => {
             <Gap height={10} />
             {/* jika erro text string must be rendered within a <Text></Text> lakukan  kondisi maksud kondisi jika fullName lebih dari 0 atau ada 1 huruf tampilkan*/}
             {profile.fullName.length > 0 && (
-                <Profile name={profile.fullName} 
-                desc={profile.profesional} 
-                photo={profile.photo}
-                 />
+                <Profile name={profile.fullName}
+                    desc={profile.profesional}
+                    photo={profile.photo}
+                />
             )}
             <Gap height={14} />
             <List desc="Last Update yesterday" type="next" name="Edit Profile" icon="edit-profile" onPrees={() => navigation.navigate('UpdateProfile')} />
             <List desc="Last Update yesterday" type="next" name="language" icon="language" />
             <List desc="Last Update yesterday" type="next" name="Rate" icon="rate" />
-            <List desc="Last Update yesterday" type="next" name="Help" icon="help" />
+            <List type="next" name="Sign Out" icon="help" onPrees={singOut} />
         </View>
     )
 }
